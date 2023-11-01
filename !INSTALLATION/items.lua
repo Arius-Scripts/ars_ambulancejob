44ea3f0b39f38401b62843f8bbf9a861f4d46baf
Arius <70983185+Arius-Development@users.noreply.github.com>
2023-11-01 13:34:18 +0100

    created item to open distress calls

--- !INSTALLATION/items.lua
+++ !INSTALLATION/items.lua
@@ -1,42 +1,42 @@
 ['medicalbag'] = {
     label = 'Medical Bag',
     weight = 220,
-    stack = false,
+    stack = true,
     description = "A comprehensive medical kit for treating injuries and ailments.",
 },
 
 ['bandage'] = {
     label = 'Bandage',
     weight = 100,
-    stack = false,
+    stack = true,
     description = "A simple bandage used to cover and protect wounds.",
 },
 
 ['defibrillator'] = {
     label = 'Defibrillator',
     weight = 100,
-    stack = false,
+    stack = true,
     description = "Used for reviving patients.",
 },
 
 ['tweezers'] = {
     label = 'Tweezers',
     weight = 100,
-    stack = false,
+    stack = true,
     description = "Precision tweezers for safely removing foreign objects, such as bullets, from wounds.",
 },
 
 ['burncream'] = {
     label = 'Burn Cream',
     weight = 100,
-    stack = false,
+    stack = true,
     description = "Specialized cream for treating and soothing minor burns and skin irritations.",
 },
 
 ['suturekit'] = {
     label = 'Suture Kit',
     weight = 100,
-    stack = false,
+    stack = true,
     description = "A kit containing surgical tools and materials for stitching and closing wounds.",
 },
 
@@ -45,4 +45,20 @@
     weight = 200,
     stack = true,
     description = "An ice pack used to reduce swelling and provide relief from pain and inflammation.",
+},
+
+['stretcher'] = {
+    label = 'Ice Pack',
+    weight = 200,
+    stack = true,
+    description = "An ice pack used to reduce swelling and provide relief from pain and inflammation.",
+},
+
+['emstablet'] = {
+    label = 'Ems tablet',
+    weight = 200,
+    stack = true,
+    client = {
+        exports.ars_ambulancejob:openDistressCalls()
+    }
 },
