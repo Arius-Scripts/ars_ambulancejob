1b9a0f5633818703eab93804d3d42de19db95b1c
Arius <70983185+Arius-Development@users.noreply.github.com>
2023-11-01 12:15:29 +0100

    created installation folder

---
+++ !INSTALLATION/items.lua
@@ -0,0 +1,48 @@
+['medicalbag'] = {
+    label = 'Medical Bag',
+    weight = 220,
+    stack = false,
+    description = "A comprehensive medical kit for treating injuries and ailments.",
+},
+
+['bandage'] = {
+    label = 'Bandage',
+    weight = 100,
+    stack = false,
+    description = "A simple bandage used to cover and protect wounds.",
+},
+
+['defibrillator'] = {
+    label = 'Defibrillator',
+    weight = 100,
+    stack = false,
+    description = "Used for reviving patients.",
+},
+
+['tweezers'] = {
+    label = 'Tweezers',
+    weight = 100,
+    stack = false,
+    description = "Precision tweezers for safely removing foreign objects, such as bullets, from wounds.",
+},
+
+['burncream'] = {
+    label = 'Burn Cream',
+    weight = 100,
+    stack = false,
+    description = "Specialized cream for treating and soothing minor burns and skin irritations.",
+},
+
+['suturekit'] = {
+    label = 'Suture Kit',
+    weight = 100,
+    stack = false,
+    description = "A kit containing surgical tools and materials for stitching and closing wounds.",
+},
+
+['icepack'] = {
+    label = 'Ice Pack',
+    weight = 200,
+    stack = true,
+    description = "An ice pack used to reduce swelling and provide relief from pain and inflammation.",
+},
--- README.md
+++ README.md
@@ -1,10 +1,6 @@
 ```lua
 
-    ['medicalbag'] = {
-        label = 'Medical bag',
-        weight = 220,
-        stack = false,
-    }
+
 
 
 ```
