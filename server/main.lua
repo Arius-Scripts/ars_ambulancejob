8e8fc88ed903b1ce898886adf3eca378c95d1dae
Arius <70983185+Arius-Development@users.noreply.github.com>
2023-11-04 17:41:11 +0100

    arranging code

--- server/main.lua
+++ server/main.lua
@@ -10,10 +10,6 @@ RegisterNetEvent("ars_ambulancejob:updateDeathStatus", function(death)
     updateStatus(data)
 end)
 
-
-
-
-
 RegisterNetEvent("ars_ambulancejob:revivePlayer", function(data)
     if not hasJob(source, Config.EmsJobs) or not source or source < 1 then return end
 
@@ -52,22 +48,6 @@ RegisterNetEvent("ars_ambulancejob:healPlayer", function(data)
     end
 end)
 
-
-
-lib.callback.register('ars_ambulancejob:getDeathStatus', function(source, target)
-    return player[target] and player[target] or getDeathStatus(target or source)
-end)
-
-lib.callback.register('ars_ambulancejob:getData', function(source, target)
-    local data = {}
-    data.injuries = Player(target).state.injuries or false
-    data.status = getDeathStatus(target or source) or Player(target).state.dead
-    data.killedBy = player[target]?.killedBy or false
-
-    return data
-end)
-
-
 RegisterNetEvent("ars_ambulancejob:createDistressCall", function(data)
     if not source or source < 1 then return end
     distressCalls[#distressCalls + 1] = {
@@ -97,24 +77,6 @@ RegisterNetEvent("ars_ambulancejob:callCompleted", function(call)
     end
 end)
 
-lib.callback.register('ars_ambulancejob:getDistressCalls', function(source)
-    return distressCalls
-end)
-
-lib.callback.register('ars_ambulancejob:openMedicalBag', function(source)
-    exports.ox_inventory:RegisterStash("medicalBag_" .. source, "Medical Bag", 10, 50 * 1000)
-
-    return "medicalBag_" .. source
-end)
-
-
-exports.ox_inventory:registerHook('swapItems', function(payload)
-    if string.find(payload.toInventory, "medicalBag_") then
-        if payload.fromSlot.name == Config.MedicBagItem then return false end
-    end
-end, {})
-
-
 RegisterNetEvent("ars_ambulancejob:removAddItem", function(data)
     if not hasJob(source, Config.EmsJobs) then return end
 
@@ -134,7 +96,6 @@ RegisterNetEvent("ars_ambulancejob:useItem", function(data)
     exports.ox_inventory:SetDurability(source, slot, item.metadata?.durability and (item.metadata?.durability - data.value) or (100 - data.value))
 end)
 
-
 RegisterNetEvent("ars_ambulancejob:removeInventory", function()
     if player[source].isDead and Config.RemoveItemsOnRespawn then
         exports.ox_inventory:ClearInventory(source)
@@ -153,7 +114,34 @@ RegisterNetEvent("ars_ambulancejob:togglePatientFromVehicle", function(data)
     TriggerClientEvent("ars_ambulancejob:togglePatientFromVehicle", data.target, data.vehicle)
 end)
 
+lib.callback.register('ars_ambulancejob:getDeathStatus', function(source, target)
+    return player[target] and player[target] or getDeathStatus(target or source)
+end)
 
+lib.callback.register('ars_ambulancejob:getData', function(source, target)
+    local data = {}
+    data.injuries = Player(target).state.injuries or false
+    data.status = getDeathStatus(target or source) or Player(target).state.dead
+    data.killedBy = player[target]?.killedBy or false
+
+    return data
+end)
+
+lib.callback.register('ars_ambulancejob:getDistressCalls', function(source)
+    return distressCalls
+end)
+
+lib.callback.register('ars_ambulancejob:openMedicalBag', function(source)
+    exports.ox_inventory:RegisterStash("medicalBag_" .. source, "Medical Bag", 10, 50 * 1000)
+
+    return "medicalBag_" .. source
+end)
+
+exports.ox_inventory:registerHook('swapItems', function(payload)
+    if string.find(payload.toInventory, "medicalBag_") then
+        if payload.fromSlot.name == Config.MedicBagItem then return false end
+    end
+end, {})
 
 AddEventHandler('onServerResourceStart', function(resourceName)
     if resourceName == GetCurrentResourceName() then
